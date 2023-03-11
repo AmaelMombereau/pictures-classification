ee5fcdc500a69576c81e494477f5e56689b69bc8
Amaël Mombereau <110431786+AmaelMombereau@users.noreply.github.com>
2023-03-11 20:22:49 +0100

    Update P6_app.py
    Présentation en anglais

--- P6_app.py
+++ P6_app.py
@@ -31,26 +31,26 @@ def get_prediction(model, img, class_names):
 
 def main():
     model, class_names = load_model_and_class_names()
-    st.title("Détection de la race d'un chien depuis une photo")
-    file = st.file_uploader("Veuillez charger une image")
+    st.title("Dog Breed Prediction from a Picture")
+    file = st.file_uploader("Load a picture of your dog")
     img_placeholder = st.empty()
     success = st.empty()
     submit_placeholder = st.empty()
     submit=False
 
     if file is not None:
-        with st.spinner("hargement de l'image.."):  
+        with st.spinner("Loading..."):  
             model, class_names = load_model_and_class_names()
             img = Image.open(file)
             img_placeholder.image(img, width=299)
-        submit = submit_placeholder.button("Lancer la détection de race")
+        submit = submit_placeholder.button("Launch prediction")
 
     if submit:
-        with st.spinner('Résultat en attente...'):    
+        with st.spinner('Waiting for result...'):    
             submit_placeholder.empty()
             img_tensor = load_image(img)
             res = get_prediction(model=model, img=img_tensor, class_names=class_names)
-            success.success("Pour le modèle il s'agit d'un {}".format(res))
+            success.success("Breed: {}".format(res))
 
 if __name__ == "__main__":
     main()
